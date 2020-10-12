325d2c816dc800638aec898f52f9f97228db0089
tunght91 <hoangtung.utc@gmail.com>
2020-10-12 10:38:49 +0700

    update active

--- luaswap/getPrice.js
+++ luaswap/getPrice.js
@@ -8,24 +8,45 @@ async function getPrice(token) {
     return PRICE[token].value || 0;
   }
 
-  if (token === 'LUA') {
+  if (token === 'USDC' || token === 'USDCT' || token === '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' || token == '0xdac17f958d2ee523a2206206994597c13d831ec7') {
+    return 1
+  }
+
+  if (token === 'LUA' || token === '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc') {
     var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=lua-token&vs_currencies=usd')
     price = parseFloat(data['lua-token'].usd) || 0
   }
-  else if (token === 'ETH') {
+  else if (token === 'ETH' || token === '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2') {
     var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
     price = parseFloat(data['ethereum'].usd) || 0
   }
-  else if (token === 'SUSHI') {
+  else if (token === 'SUSHI' || token === '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2') {
     var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=sushi&vs_currencies=usd')
     price = parseFloat(data['sushi'].usd) || 0
   }
-  else if (token === 'TOMO' || token === 'TOMOE') {
+  else if (token === 'TOMO' || token === 'TOMOE' || token === '0x05d3606d5c81eb9b7b18530995ec9b29da05faba') {
     var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=tomochain&vs_currencies=usd')
     price = parseFloat(data['tomochain'].usd) || 0
   }
-  else if (token === 'FRONT') {
-    price = 0.3
+  else if (token === 'SRM' || token === '0x476c5e26a75bd202a9683ffd34359c0cc15be0ff') {
+    var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=serum&vs_currencies=usd')
+    price = parseFloat(data['serum'].usd) || 0
+  }
+  else if (token === 'FTT' || token === '0x50d1c9771902476076ecfc8b2a83ad6b9355a4c9') {
+    var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ftx-token&vs_currencies=usd')
+    price = parseFloat(data['ftx-token'].usd) || 0
+  }
+  else if (token === 'KAI' || token === '0xd9ec3ff1f8be459bb9369b4e79e9ebcf7141c093') {
+    var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=kardiachain&vs_currencies=usd')
+    price = parseFloat(data['kardiachain'].usd) || 0
+  }
+  else if (token === 'OM' || token === '0x2baecdf43734f22fd5c152db08e3c27233f0c7d2') {
+    var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=mantra-dao&vs_currencies=usd')
+    price = parseFloat(data['mantra-dao'].usd) || 0
+  }
+  else if (token === 'FRONT' || token === '0xf8c3527cc04340b208c854e985240c02f7b7793f') {
+    var { data } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=frontier-token&vs_currencies=usd')
+    price = parseFloat(data['frontier-token'].usd) || 0
   }
   else {
     price = 0
--- luaswap/pools.js
+++ luaswap/pools.js
@@ -192,6 +192,151 @@ var supportedPools = [
     pairLink: 'https://uniswap.info/pair/0xfe1ead71b27e8389d819ee0a420080d90a60132c',
     addLiquidityLink: 'https://app.uniswap.org/#/add/0x6b3595068778dd592e39a122f4f5a5cf09c90fe2/0xb1f66997a5760428d3a87d68b90bfe0ae64121cc'
   },
+
+  {
+    startAt: 1602565208,
+    pid: 7,
+    lpAddresses: {
+      1: '0x694ad474ef16a8eefb5cc3119f9956aeef28c987',
+    },
+    tokenAddresses: {
+      1: '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc',
+    },
+
+    token2Addresses: {
+      1: '0x476c5e26a75bd202a9683ffd34359c0cc15be0ff'
+    },
+    name: 'LUA - SRM',
+    symbol: 'LUA-SRM UNI-V2 LP',
+    symbolShort: 'LUA-SRM',
+    description: `Deposit LUA-SRM UNI-V2 LP Earn LUA`,
+    tokenSymbol: 'LUA',
+    token2Symbol: 'SRM',
+    icon: 'https://luaswap.org/favicon.png',
+    icon2: 'https://s2.coinmarketcap.com/static/img/coins/128x128/6187.png',
+    isHot: false,
+    isNew: true,
+    protocal: 'UniSwap',
+    iconProtocal: 'https://uniswap.info/static/media/logo_white.edb44e56.svg',
+    pairLink: 'https://uniswap.info/pair/0x694ad474ef16a8eefb5cc3119f9956aeef28c987',
+    addLiquidityLink: 'https://app.uniswap.org/#/add/0x476c5e26a75bd202a9683ffd34359c0cc15be0ff/0xb1f66997a5760428d3a87d68b90bfe0ae64121cc'
+  },
+
+  {
+    startAt: 1602565208,
+    pid: 8,
+    lpAddresses: {
+      1: '0xf0ec5e8ea37911dec8e8e9bc940e9dba2de60706',
+    },
+    tokenAddresses: {
+      1: '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc',
+    },
+
+    token2Addresses: {
+      1: '0x50d1c9771902476076ecfc8b2a83ad6b9355a4c9'
+    },
+    name: 'LUA - FTT',
+    symbol: 'LUA-FTT UNI-V2 LP',
+    symbolShort: 'LUA-FTT',
+    description: `Deposit LUA-FTT UNI-V2 LP Earn LUA`,
+    tokenSymbol: 'LUA',
+    token2Symbol: 'FTT',
+    icon: 'https://luaswap.org/favicon.png',
+    icon2: 'https://s2.coinmarketcap.com/static/img/coins/128x128/4195.png',
+    isHot: false,
+    isNew: true,
+    protocal: 'UniSwap',
+    iconProtocal: 'https://uniswap.info/static/media/logo_white.edb44e56.svg',
+    pairLink: 'https://info.uniswap.org/pair/0xf0ec5e8ea37911dec8e8e9bc940e9dba2de60706',
+    addLiquidityLink: 'https://app.uniswap.org/#/add/0x50d1c9771902476076ecfc8b2a83ad6b9355a4c9/0xb1f66997a5760428d3a87d68b90bfe0ae64121cc'
+  },
+
+  {
+    startAt: 1602565208,
+    pid: 9,
+    lpAddresses: {
+      1: '0xc5d3c66133a6264b0f2e712b8e10ef96efb93eb2',
+    },
+    tokenAddresses: {
+      1: '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc',
+    },
+
+    token2Addresses: {
+      1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+    },
+    name: 'LUA - ETH',
+    symbol: 'LUA-ETH UNI-V2 LP',
+    symbolShort: 'LUA-ETH',
+    description: `Deposit LUA-ETH UNI-V2 LP Earn LUA`,
+    tokenSymbol: 'LUA',
+    token2Symbol: 'ETH',
+    icon: 'https://luaswap.org/favicon.png',
+    icon2: 'https://s2.coinmarketcap.com/static/img/coins/128x128/1027.png',
+    isHot: false,
+    isNew: true,
+    protocal: 'UniSwap',
+    iconProtocal: 'https://uniswap.info/static/media/logo_white.edb44e56.svg',
+    pairLink: 'https://info.uniswap.org/pair/0xc5d3c66133a6264b0f2e712b8e10ef96efb93eb2',
+    addLiquidityLink: 'https://app.uniswap.org/#/add/0xb1f66997a5760428d3a87d68b90bfe0ae64121cc/ETH'
+  },
+
+  {
+    startAt: 1602565208,
+    pid: 10,
+    lpAddresses: {
+      1: '0x9af4fb969bb16038d7618df8adbdb2e7133b0f66',
+    },
+    tokenAddresses: {
+      1: '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc',
+    },
+
+    token2Addresses: {
+      1: '0xd9ec3ff1f8be459bb9369b4e79e9ebcf7141c093'
+    },
+    name: 'LUA - KAI',
+    symbol: 'LUA-KAI UNI-V2 LP',
+    symbolShort: 'LUA-KAI',
+    description: `Deposit LUA-KAI UNI-V2 LP Earn LUA`,
+    tokenSymbol: 'LUA',
+    token2Symbol: 'KAI',
+    icon: 'https://luaswap.org/favicon.png',
+    icon2: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xD9Ec3ff1f8be459Bb9369b4E79e9Ebcf7141C093/logo.png',
+    isHot: false,
+    isNew: true,
+    protocal: 'UniSwap',
+    iconProtocal: 'https://uniswap.info/static/media/logo_white.edb44e56.svg',
+    pairLink: 'https://info.uniswap.org/pair/0x9af4fb969bb16038d7618df8adbdb2e7133b0f66',
+    addLiquidityLink: 'https://app.uniswap.org/#/add/0xb1f66997a5760428d3a87d68b90bfe0ae64121cc/0xd9ec3ff1f8be459bb9369b4e79e9ebcf7141c093'
+  },
+
+  {
+    startAt: 1602565208,
+    pid: 11,
+    lpAddresses: {
+      1: '0x9ccb79d6523152aee4dc2be5822fdbafd0d63211',
+    },
+    tokenAddresses: {
+      1: '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc',
+    },
+
+    token2Addresses: {
+      1: '0x2baecdf43734f22fd5c152db08e3c27233f0c7d2'
+    },
+    name: 'LUA - OM',
+    symbol: 'LUA-OM UNI-V2 LP',
+    symbolShort: 'LUA-OM',
+    description: `Deposit LUA-OM UNI-V2 LP Earn LUA`,
+    tokenSymbol: 'LUA',
+    token2Symbol: 'OM',
+    icon: 'https://luaswap.org/favicon.png',
+    icon2: 'https://s2.coinmarketcap.com/static/img/coins/128x128/6536.png',
+    isHot: false,
+    isNew: true,
+    protocal: 'UniSwap',
+    iconProtocal: 'https://uniswap.info/static/media/logo_white.edb44e56.svg',
+    pairLink: 'https://info.uniswap.org/pair/0x9ccb79d6523152aee4dc2be5822fdbafd0d63211',
+    addLiquidityLink: 'https://app.uniswap.org/#/add/0x2baecdf43734f22fd5c152db08e3c27233f0c7d2/0xb1f66997a5760428d3a87d68b90bfe0ae64121cc'
+  },
 ]
 
 
@@ -208,14 +353,6 @@ const getLPValue = async (
   pid,
 ) => {
   var masterChefContract = '0xb67d7a6644d9e191cac4da2b88d6817351c7ff62'
-
-  var usdtAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'
-  var usdcAddress = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-  var wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-  var tomoAddress = '0x05d3606d5c81eb9b7b18530995ec9b29da05faba'
-  var sushiAddres = '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2'
-  var frontAddress = '0xf8c3527cc04340b208c854e985240c02f7b7793f'
-  
 
   CACHE[pid] = CACHE[pid] || {
     time: 0,
@@ -264,26 +401,9 @@ const getLPValue = async (
 
     var usdValue = 0;
     var totalToken2Value = totalLpToken2Value.div(new BigNumber(10).pow(token2Decimals))
-    if (token2Contract.toLowerCase() == usdtAddress 
-      || token2Contract.toLowerCase() == usdcAddress) {
-      usdValue = totalToken2Value
-    } 
-    else if (token2Contract.toLowerCase() == wethAddress) {
-      var price = await getPrice('ETH');
-      usdValue = totalToken2Value.times(price)
-    }
-    else if (token2Contract.toLowerCase() == tomoAddress) {
-      var price = await getPrice('TOMO');
-      usdValue = totalToken2Value.times(price)
-    }
-    else if (token2Contract.toLowerCase() == frontAddress) {
-      var price = await getPrice('FRONT');
-      usdValue = totalToken2Value.times(price)
-    }
-    else if (token2Contract.toLowerCase() == sushiAddres) {
-      var price = await getPrice('SUSHI');
-      usdValue = totalToken2Value.times(price)
-    }
+
+    var price = await getPrice(token2Contract.toLowerCase());
+    usdValue = totalToken2Value.times(price)
 
     const allocPoint = (await methods
       .contract(masterChefContract)
@@ -315,7 +435,7 @@ const getLPValue = async (
 }
 
 async function getAllLPValue() {
-  return Promise.all(supportedPools.map(e => getLPValue(
+  return Promise.all(supportedPools.filter(e => active(e.pid)).map(e => getLPValue(
     e.lpAddresses[1],
     e.tokenAddresses[1],
     e.token2Addresses[1],
@@ -323,14 +443,26 @@ async function getAllLPValue() {
   )))
 }
 
+function active(pid) {
+  var e = supportedPools.find(e => e.pid == pid)
+  if (e) {
+    if (e.startAt) {
+      return e.startAt < new Date().getTime() / 1000
+    }
+    else {
+      return true
+    }
+  }
+  else {
+    return false
+  }
+}
+
 module.exports = {
   getAllLPValue,
-  active: (pid) => {
-    var e = supportedPools.find(e => e.pid == pid)
-    return !!e
-  },
+  active,
   getLPValue: (pid) => {
-    var e = supportedPools.find(e => e.pid == pid)
+    var e = active(pid)
     if (e) {
       return getLPValue(
         e.lpAddresses[1],
